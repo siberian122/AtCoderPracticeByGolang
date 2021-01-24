e0f2366b4c14f7a6dcf85fe05f975d056803acf7
siberian122 <siberian1000@gmail.com>
2021-01-24 19:47:20 +0900

    Update Orange.go

--- main/Orange.go
+++ main/Orange.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main() {
-	var n,int
+	var n int
 	var ans int =0
 	fmt.Scan(&n)
-	var a [n]int
-	fmt.Scan(&a)
+	a := make([]int, n)
+	for i:=0;i<n;i++{
+		fmt.Scan(&a[i])
+	}
 	for i:=0;i<n;i++ {
+		cnt := a[i]
 		for j:=i;j<n;j++{
-			ans=max(min(a[i:j])*(j-i),ans)
+			cnt=int(math.Min(float64(cnt),float64(a[j])))
+			ans=int(math.Max(float64(cnt*(j-i+1)),float64(ans)))
 		}
 	}
 	fmt.Println(ans)
