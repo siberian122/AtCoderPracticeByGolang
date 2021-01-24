9b41135d317fe98608269bed41ca300104e7dade
siberian122 <siberian1000@gmail.com>
2021-01-24 19:47:10 +0900

    Update Orange.go

--- main/Orange.go
+++ main/Orange.go
@@ -5,16 +5,14 @@ import (
 )
 
 func main() {
-	var n, x int
-	fmt.Scan(&n, &x)
-	var cnt float64 = 0
-	var ans int = -1
-	for i := 1; i <= n; i++ {
-		var v, p int
-		fmt.Scan(&v, &p)
-		cnt += float64(v) * float64(p/100)
-		if cnt > float64(x) {
-			ans = i + 1
+	var n,int
+	var ans int =0
+	fmt.Scan(&n)
+	var a [n]int
+	fmt.Scan(&a)
+	for i:=0;i<n;i++ {
+		for j:=i;j<n;j++{
+			ans=max(min(a[i:j])*(j-i),ans)
 		}
 	}
 	fmt.Println(ans)
